db59fed17e0b03224e2b9cd1754bfe0574ebc921
monsef <monsef@example.com>
2022-04-27 18:43:17 +0100

    add features

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+// PreLoadr
+window.addEventListener("load", (eo) => {
+  const loader = document.getElementsByClassName("loader")[0];
+  loader.classList.add("hide-loader");
+});
+
 // Nav scroll Event
 window.addEventListener("scroll", (eo) => {
   const header = document.querySelector("header");
@@ -39,26 +45,3 @@ closeMenu.addEventListener("click", (eo) => {
   openMenu.style.display = "block";
   closeMenu.style.display = "none";
 });
-// Counting Achievements
-const courses = document.getElementById("count-courses");
-const students = document.getElementById("count-students");
-const awards = document.getElementById("count-awards");
-let countC = 0;
-let countS = 0;
-let countA = 0;
-function scale(number, inMin, inMax, outMin, outMax) {
-  return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
-}
-const countAchievements = () => {
-  setInterval(() => {
-    if (countC < 450) {
-      countC++;
-      courses.innerHTML = `${countC}+`;
-      students.innerHTML = `${Math.trunc(scale(countC, 0, 450, 0, 7900))}+`;
-      awards.innerHTML = `${Math.trunc(scale(countC, 0, 450, 0, 26))}+`;
-    } else {
-      clearInterval(stop);
-    }
-  }, 0);
-};
-countAchievements();
--- js/swiper.js
+++ js/swiper.js
@@ -3,6 +3,10 @@ var swiper = new Swiper(".mySwiper", {
   spaceBetween: 20,
   centeredSlides: true,
   loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
   breakpoints: {
     320: {
       slidesPerView: 1,
